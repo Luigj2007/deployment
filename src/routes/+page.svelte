3093059bff592ba7719f89ace5319d6ff7344a47
Luigj Shala <luigjshala@Luigjs-MacBook-Pro.local>
2026-03-05 15:57:32 +0100

    nextCommit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Wir deployen unsere erste Webapp!</h1>
+<body>
+    <div>
+        <h1>Wir deployen unsere erste Webapp!</h1>
+        <p>Eine Änderung</p>
+    </div>
+</body>
 
+
+<style>
+    body{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background-color: beige;
+    }
+    div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 30px;
+        background-color: tan;
+        box-shadow: 5px;
+    }
+    h1{
+        color: rgb(99, 77, 48);
+    }
+    p{
+        color: rgb(99, 77, 48);
+    }
+</style>
